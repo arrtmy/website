97d1a3fc189d18abaff124760c1a38d142f51d64
arrtmy <159881749+arrtmy@users.noreply.github.com>
2026-07-01 11:05:52 +1200

    update style marginRight

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,8 +14,8 @@ function App() {
         <Link to="/" style={{ marginRight: 10 }}>О сервисе</Link>
         <Link to="/public-offer" style={{ marginRight: 10 }}>Публичная оферта</Link>
         <Link to="/nopublic-offer" style={{ marginRight: 10 }}>Непубличная оферта</Link>
-        <Link to="/confidentiality">Соглашение о конфиденциальности для клиентов</Link>
-        <Link to="/confidentiality-partners">Соглашение о конфиденциальности для парнеров</Link>
+        <Link to="/confidentiality" style={{ marginRight: 10 }}>Соглашение о конфиденциальности для клиентов</Link>
+        <Link to="/confidentiality-partners" style={{ marginRight: 10 }}>Соглашение о конфиденциальности для парнеров</Link>
       </nav>
       <Routes>
         <Route path="/" element={<About />} />
